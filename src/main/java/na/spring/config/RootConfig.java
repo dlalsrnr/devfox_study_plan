ebcecf402738abb0f13d9ba98940f625b94d851b
gudwlsa <gudwlsa@gmail.com>
2021-04-23 16:39:39 +0900

    기본 설정 추가

---
+++ src/main/java/na/spring/config/RootConfig.java
@@ -0,0 +1,27 @@
+package na.spring.config;
+
+import javax.sql.DataSource;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = { "na.spring.sample" })
+public class RootConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+        hikariConfig.setJdbcUrl("jdbc:oracle:thin:@localhost:1521:XE");
+        hikariConfig.setUsername("jslhrd45");
+        hikariConfig.setPassword("1234");
+
+        return new HikariDataSource(hikariConfig);
+    }
+
+}
