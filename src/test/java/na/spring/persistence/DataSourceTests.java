c43c886f883aa61c8e7b3236d33fc2ac27964cad
gudwlsa <gudwlsa@gmail.com>
2021-04-23 16:40:12 +0900

    OracleDB 연결, HikariCP 연결 및 테스트

---
+++ src/test/java/na/spring/persistence/DataSourceTests.java
@@ -0,0 +1,34 @@
+package na.spring.persistence;
+
+import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j2;
+import na.spring.config.RootConfig;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { RootConfig.class })
+@Log4j2
+public class DataSourceTests {
+    @Setter(onMethod_ = { @Autowired })
+    private DataSource dataSource;
+
+    @Test
+    public void testConnection() {
+        try (Connection con = dataSource.getConnection()) {
+            log.info(con);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+}
---
+++ src/test/java/na/spring/persistence/JDBCTests.java
@@ -0,0 +1,30 @@
+package na.spring.persistence;
+
+import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class JDBCTests {
+    static {
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testConnection() {
+        try (Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "jslhrd45", "1234")) {
+            log.info(con);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+}
