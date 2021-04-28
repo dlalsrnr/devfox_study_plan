fff7adc4f0054619423eb5ee70023fa00f966663
gudwlsa <gudwlsa@gmail.com>
2021-04-28 12:55:32 +0900

    디렉토리 구조 변경

---
+++ part1/src/test/java/na/spring/part1/Part1ApplicationTests.java
@@ -0,0 +1,13 @@
+package na.spring.part1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Part1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
