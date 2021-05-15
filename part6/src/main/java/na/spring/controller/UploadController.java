782bb2df5d7e7322b4b04f355b10992fce443c0b
gudwlsa <gudwlsa@gmail.com>
2021-05-15 15:41:38 +0900

    년/월/일 경로 추가, uuid를 통한 이름중복 해결

--- part6/src/main/java/na/spring/controller/UploadController.java
+++ part6/src/main/java/na/spring/controller/UploadController.java
@@ -1,6 +1,9 @@
 package na.spring.controller;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -42,16 +45,28 @@ public class UploadController {
     @PostMapping("/uploadAjaxAction")
     public void uploadAjaxPost(MultipartFile[] uploadFile) {
         String uploadFolder = "C:/upload"; // 경로는 / or \\ 둘 다 사용가능
+        File uploadPath = new File(uploadFolder, getFolder());
+        log.info("upload path : " + uploadPath);
+        if (!uploadPath.exists()) // 경로에 폴더가 없으면 만듬
+            uploadPath.mkdirs();
         for (MultipartFile multipartFile : uploadFile) {
             log.info("------------------------------------------");
             log.info("Upload File Name : " + multipartFile.getOriginalFilename());
             log.info("Upload File Size : " + multipartFile.getSize());
-            File saveFile = new File(uploadFolder, multipartFile.getOriginalFilename());
+            UUID uuid = UUID.randomUUID();
+            File saveFile = new File(uploadPath, (uuid.toString() + "_" + multipartFile.getOriginalFilename()));
             try {
                 multipartFile.transferTo(saveFile);
             } catch (Exception e) {
                 log.error(e.getMessage());
             }
         }
+    }
+
+    private String getFolder() {
+        SimpleDateFormat sFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = new Date();
+        String str = sFormat.format(date);
+        return str.replace("-", File.separator);
     }
 }
--- part6/src/main/webapp/WEB-INF/views/uploadAjax.jsp
+++ part6/src/main/webapp/WEB-INF/views/uploadAjax.jsp
@@ -15,12 +15,27 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script>
         $(function() {
+            const regex = new RegExp("(.*?)\.(exe|sh|zip|alz)$")
+            const maxSize = 10*1024*1024 // 10MB
+            function checkExtenstion(fileName, fileSize) {
+                if (fileSize >= maxSize) {
+                    alert("10MB 이하의 파일만 업로드 할 수 있습니다.")
+                    return false
+                }
+                if (regex.test(fileName)) {
+                    alert("업로드 할 수 없는 파일 타입입니다.")
+                    return false
+                }
+                return true
+            }
             $("#uploadBtn").click(function(e) {
                 const formData = new FormData()
                 const inputFile = $("input[name='uploadFile']")
                 const files = inputFile[0].files
                 console.log(files)
                 for (let i = 0; i < files.length; i++) {
+                    if (!checkExtenstion(files[i].name, files[i].size))
+                        return false
                     formData.append("uploadFile", files[i]);
                 }
                 $.ajax({
