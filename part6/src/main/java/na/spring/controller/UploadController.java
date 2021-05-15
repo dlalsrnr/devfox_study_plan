99b3978917ff997b5194fd50d4e405c33afaba38
gudwlsa <gudwlsa@gmail.com>
2021-05-15 14:31:26 +0900

    form을 사용하는 파일 업로드

---
+++ part6/src/main/java/na/spring/controller/UploadController.java
@@ -0,0 +1,36 @@
+package na.spring.controller;
+
+import java.io.File;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+public class UploadController {
+    @GetMapping("/uploadForm")
+    public void uploadForm() {
+        log.info("upload form");
+    }
+
+    @PostMapping(value = "/uploadFormAction")
+    public void uploadFormPost(MultipartFile[] uploadFile, Model model) {
+        String uploadFolder = "C:/upload"; // 경로는 / or \\ 둘 다 사용가능
+        for (MultipartFile multipartFile : uploadFile) {
+            log.info("------------------------------------------");
+            log.info("Upload File Name : " + multipartFile.getOriginalFilename());
+            log.info("Upload File Size : " + multipartFile.getSize());
+            File saveFile = new File(uploadFolder, multipartFile.getOriginalFilename());
+            try {
+                multipartFile.transferTo(saveFile);
+            } catch (Exception e) {
+                log.error(e.getMessage());
+            }
+        }
+    }
+}
