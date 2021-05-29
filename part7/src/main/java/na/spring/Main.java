9a0b80d19bcc4d5705b84243802abf0a588ac2fc
gudwlsa <gudwlsa@gmail.com>
2021-05-29 18:00:43 +0900

    어노테이션을 이용하는 설정

--- part7/src/main/java/na/spring/Main.java
+++ part7/src/main/java/na/spring/Main.java
@@ -2,8 +2,10 @@ package na.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class Main {
 
 	public static void main(String[] args) {
