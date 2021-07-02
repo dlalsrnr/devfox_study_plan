1d44b8f8a7a5a2b6859e66449e38cd2e330b0b25
gudwlsa <gudwlsa@gmail.com>
2021-07-02 12:50:36 +0900

    part3

--- react200/client/src/App.js
+++ react200/client/src/App.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import Promise from './R065_Promise';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import StrAddButton from './StrAddButton';
 
-function App() {
-  return (
-    <div>
-      <h1>Start React 200!</h1>
-      <Promise />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Start React 200!</h1>
+        {/* <span>{this.props.store.getState().data.str}</span><br/> */}
+        <span>{this.props.str}</span>
+        <br />
+        {/* <StrAddButton store={this.props.store}/> */}
+        <StrAddButton AppProp="200" />
+      </div>
+    );
+  }
 }
+
+let mapStateToProps = (state, props) => {
+  console.log('Props from indes.js : ' + props.indexProp);
+  return {
+    str: state.data.str,
+  };
+};
+
+App = connect(mapStateToProps, null)(App);
 
 export default App;
