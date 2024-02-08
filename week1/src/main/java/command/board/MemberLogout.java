1e9ad9d4575de5b22f458c9e904487cd8eabcb16
MinGukLee <93499338+dlalsrnr@users.noreply.github.com>
2024-02-08 19:26:13 +0900

    Create MemberLogout.java

---
+++ week1/src/main/java/command/board/MemberLogout.java
@@ -0,0 +1,23 @@
+package command.board;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import common.CommonExcute;
+
+public class MemberLogout implements CommonExcute {
+
+	@Override
+	public void excute(HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		String name = (String)session.getAttribute("sessionName");
+		session.invalidate();
+		
+		String msg = name+"님 로그아웃 되었습니다!";
+		if(name == null) msg = "로그아웃 되었습니다!";
+		
+		request.setAttribute("t_msg", msg);
+		request.setAttribute("t_url", "freeboard");
+	}
+
+}
