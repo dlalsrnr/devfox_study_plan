621bbc184b1c679c8ea926f6eac972ad2b97a5b6
MinGukLee <93499338+dlalsrnr@users.noreply.github.com>
2024-02-16 16:58:12 +0900

    4주차 업데이트
    4주차 업데이트

--- week1/src/main/java/command/board/BoardView.java
+++ week1/src/main/java/command/board/BoardView.java
@@ -13,10 +13,14 @@ public class BoardView implements CommonExcute {
 	@Override
 	public void excute(HttpServletRequest request) {
 		BoardDao dao = new BoardDao();
+		String sessionName = dao.getSessionName(request);
 		String no = request.getParameter("t_no");
 		BoardDto dto = dao.getBoardView(no);
 		ArrayList<BoardDto> dtos = dao.getCommentList(no);
+		String cno = "";
+		BoardDto dto2 = dao.getCommentView(sessionName,cno);
 		request.setAttribute("dto", dto);
+		request.setAttribute("dto2", dto2);
 		request.setAttribute("dtos", dtos);
 	}
 
