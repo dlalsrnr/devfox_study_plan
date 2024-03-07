26068c013128d5329cf48cc5617f4b8aa99a4300
MinGukLee <93499338+dlalsrnr@users.noreply.github.com>
2024-03-08 04:03:09 +0900

    Update FreeboardController.java

--- week1/src/main/java/co/spring/freeboard/FreeboardController.java
+++ week1/src/main/java/co/spring/freeboard/FreeboardController.java
@@ -1,25 +1,65 @@
-package co.spring.freeboard;
+package co.spring.test;
 
+
+import java.util.ArrayList;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import boardDao.BoardDao;
+import boardDto.BoardDto;
 import command.board.BoardDelete;
 import command.board.BoardList;
 import command.board.BoardSave;
 import command.board.BoardUpdate;
 import command.board.BoardView;
+import command.board.CommentDelete;
 import command.board.CommentSave;
+import command.board.CommentUpdate;
+import command.board.CommentUpdateForm;
 import command.board.MemberLogin;
 import command.board.MemberLogout;
 import command.board.MemberSave;
 import common.CommonExcute;
+import common.CommonUtil;
 
 @Controller
 public class FreeboardController {
-
+	
+	
+	@GetMapping(value = "/commentList", produces = "application/json")
+	@ResponseBody
+	public ArrayList<BoardDto> commentList(@RequestParam("t_no") String t_no){
+		BoardDao dao = new BoardDao();
+		ArrayList<BoardDto> dtos = new ArrayList<>();
+		dtos = dao.getCommentList(t_no);
+		return dtos;
+	}
+	
+	
+	@PostMapping(value = "/commentSave", produces = "application/json")
+	@ResponseBody
+	public int commentSave(@RequestParam("t_no") int t_no, @RequestParam("comment") String comment, HttpServletRequest request) {
+		BoardDao dao = new BoardDao();
+		String reg_id = dao.getSessionName(request);
+		String reg_date = CommonUtil.getToday();
+		int no = dao.getMaxNo();
+		int result = dao.commentSave2(t_no,comment,reg_id,reg_date,no);
+		return result;
+	}
+	
+	@PostMapping(value = "/checkid", produces = "application/json")
+	@ResponseBody
+	public  int idCheck(@RequestParam("mid") String mid) {
+		return BoardDao.checkId(mid);
+	}
+	
 	@RequestMapping("freeboard")
 	public String boardList(HttpServletRequest request) {
 		String gubun = request.getParameter("t_gubun");
@@ -32,6 +72,8 @@ public class FreeboardController {
 			viewPage = "/board_list";
 			
 		} else if(gubun.equals("writeForm")) {
+			String today = CommonUtil.getToday();
+			request.setAttribute("today", today);
 			viewPage = "/board_write";
 			
 		} else if(gubun.equals("boardSave")) {
@@ -65,6 +107,24 @@ public class FreeboardController {
 			board.excute(request);
 			viewPage = "common_alert_view";
 		
+		//댓글수정화면	
+		} else if(gubun.equals("commentUpdateForm")) {
+			CommonExcute board = new CommentUpdateForm();
+			board.excute(request);
+			viewPage = "/comment_update";
+			
+		//댓글수정	
+		} else if(gubun.equals("commentUpdate")) {
+			CommonExcute board = new CommentUpdate();
+			board.excute(request);
+			viewPage = "common_alert_view";
+			
+		//댓글삭제	
+		} else if(gubun.equals("commentDelete")) {
+			CommonExcute board = new CommentDelete();
+			board.excute(request);
+			viewPage = "common_alert_view";
+		
 		//로그인 화면
 		} else if(gubun.equals("memberLogin")) {
 			viewPage = "member_login";
@@ -83,6 +143,8 @@ public class FreeboardController {
 			
 		//회원가입 화면
 		} else if(gubun.equals("memberJoin")) {
+			String today = CommonUtil.getToday();
+			request.setAttribute("today", today);
 			viewPage = "member_join";
 			
 		//회원저장
